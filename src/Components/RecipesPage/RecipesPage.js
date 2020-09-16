11ea761efc0f9d0311d03f9c6c544a8383dca8cb
Carly Clift <carlymclift@gmail.com>
2020-09-16 17:12:58 -0600

    Create files

---
+++ src/Components/RecipesPage/RecipesPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './RecipesPage.css'
+
+class RecipesPage extends Component {
+  render() {
+    return (
+      <section className="main-section" id="vodka">
+      </section>
+    )
+  }
+}
+
+export default RecipesPage
