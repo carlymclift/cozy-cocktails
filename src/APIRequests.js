ee6e920aa72c6519d64757ac6b8210e883219bb0
Carly Clift <carlymclift@gmail.com>
2020-09-17 16:01:42 -0600

    Delete api key for security while pushing

--- src/APIRequests.js
+++ src/APIRequests.js
@@ -1,11 +1,11 @@
 export const getAllRecipes = async () => {
-  const response = await fetch('https://www.thecocktaildb.com/api/json/v2/9973533/popular.php')
+  const response = await fetch('https://www.thecocktaildb.com/')
   const data = await response.json()
   return data
 }
 
 export const getIndividualDrinkDetails = async (id) => {
-  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+  const response = await fetch(`https://www.thecocktaildb.com/)
   const data = await response.json()
   return data
 }
