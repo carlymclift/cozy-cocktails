9aa067cc11944038878b693a3279761ab0f66960
Carly Clift <carlymclift@gmail.com>
2020-09-17 17:35:02 -0600

    Import jest to test file

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import { MemoryRouter } from 'react-router-dom'
+import '@testing-library/jest-dom'
+import { getAllRecipes } from './APIRequests'
+jest.mock('./APIRequests')
 
 describe('App', () => {
 
@@ -17,6 +20,13 @@ describe('App', () => {
     expect(screen.getByRole('heading', { name: 'Cozy Cocktails'})).toBeInTheDocument()
     expect(screen.getByRole('link', { name: 'Home'})).toBeInTheDocument()
     expect(screen.getByRole('link', { name: 'Login'})).toBeInTheDocument()
+    expect(screen.getAllByRole('button')).toHaveLength(14)
     expect(screen.getAllByRole('link')).toHaveLength(3)
+  })
+
+  it('Should render a list of popular drinks when the app loads', () => {
+    getAllRecipes.mockResolvedValueOnce({
+      drinks: 
+    })
   })
 })
