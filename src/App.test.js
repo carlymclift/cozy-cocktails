9262da7ba332e988af5c9bbc07cf136a5c30f1d0
Carly Clift <carlymclift@gmail.com>
2020-09-17 17:24:30 -0600

    Start testing file with unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import { MemoryRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+
+  beforeEach( () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+  })
+
+  it('Should render the correct content', () => {
+    expect(screen.getByRole('heading', { name: 'Cozy Cocktails'})).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: 'Home'})).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: 'Login'})).toBeInTheDocument()
+    expect(screen.getAllByRole('link')).toHaveLength(3)
+  })
+})
