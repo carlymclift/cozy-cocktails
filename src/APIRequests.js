2ce209f9ea544d2a2e0eefe354c463e96e76885f
Carly Clift <carlymclift@gmail.com>
2020-09-18 11:19:21 -0600

    Write fetch for getting a random drink

--- src/APIRequests.js
+++ src/APIRequests.js
@@ -6,6 +6,12 @@ export const getAllRecipes = async () => {
   return data
 }
 
+export const getRandomDrink = async () => {
+  const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+  const data = await response.json()
+  return data
+}
+
 export const getIndividualDrinkDetails = async (id) => {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v2/${apiKey}/lookup.php?i=${id}`)
   const data = await response.json()
