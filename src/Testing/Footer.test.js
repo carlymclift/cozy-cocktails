6634c93eb804ba85286c2407951ed77173871de8
Carly Clift <carlymclift@gmail.com>
2020-09-21 15:29:39 -0600

    Take out unused imports

--- src/Testing/Footer.test.js
+++ src/Testing/Footer.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { render, screen, waitFor} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Footer from '../Components/Footer/Footer'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 
-describe('Header', () => {
+describe('Footer', () => {
 
   beforeEach( () => {
     render(
